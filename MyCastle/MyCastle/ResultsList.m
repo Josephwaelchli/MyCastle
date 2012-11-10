8e5c566b2e56ea10e854db3b26233007a408f8a9
Brandon Salter <brandon.salter@cuw.edu>
2012-11-10 12:27:27 -0600

    asdf

---
+++ MyCastle/MyCastle/ResultsList.m
@@ -0,0 +1,38 @@
+//
+//  ResultsList.m
+//  MyCastle
+//
+//  Created by Brandon Salter on 11/10/12.
+//  Copyright (c) 2012 Pwn Industries. All rights reserved.
+//
+
+#import "ResultsList.h"
+
+@interface ResultsList ()
+
+@end
+
+@implementation ResultsList
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
