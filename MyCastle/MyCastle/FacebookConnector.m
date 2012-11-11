3a61607ed5f255d10c92e7f866e94792afad40f5
Jopseh Waelchli <Joseph.Waelchli@cuw.edu>
2012-11-11 09:52:18 -0600

    pushing fb.

--- MyCastle/MyCastle/FacebookConnector.m
+++ MyCastle/MyCastle/FacebookConnector.m
@@ -34,19 +34,21 @@
 //weird facebook methods taken from their examples lol. 
 -(void)sessionStateChanged:(FBSession *)session state:(FBSessionState) state error:(NSError *)error
 {
-    AppDelegate* theAppDel = (AppDelegate*)[[UIApplication sharedApplication] delegate];
+    NSLog(@"yay");
+    //AppDelegate* theAppDel = (AppDelegate*)[[UIApplication sharedApplication] delegate];
     switch (state) {
         case FBSessionStateOpen: {
-            
+            //[[FBSession activeSession] state] = FBSessionStateOpen;
+            NSLog(@"yay");
         }
             break;
         case FBSessionStateClosed:
         case FBSessionStateClosedLoginFailed:
             // Once the user has logged in, we want them to
             // be looking at the root view.
-            [theAppDel.nc popToRootViewControllerAnimated:NO];
-            
-            [FBSession.activeSession closeAndClearTokenInformation];
+            ///[theAppDel.nc popToRootViewControllerAnimated:NO];
+            NSLog(@"bad");
+            //[FBSession.activeSession closeAndClearTokenInformation];
             
             //[self showLoginView];
             break;
@@ -68,11 +70,31 @@
 //also helps log into facebook.
 -(void)openSession
 {
+    NSArray *permissions =
+    [NSArray arrayWithObjects:@"email", nil];
+    
+    [FBSession openActiveSessionWithReadPermissions:permissions
+                                       allowLoginUI:YES
+                                  completionHandler:^(FBSession *session, FBSessionState status, NSError *error) {
+                                      /* handle success + failure in block */
+                                      [self sessionStateChanged:session state:status error:error];
+                                  }];
+    /*[FBSession openActiveSessionWithReadPermissions:nil
+                                       allowLoginUI:YES
+                                  completionHandler:^(FBSession *session, FBSessionState state, NSError *error) {
+                                      [self sessionStateChanged:session state:state error:error];
+                                  }];
+    [FBSession openActiveSessionWithReadPermissions:nil
+                                       allowLoginUI:YES
+                                  completionHandler:^(FBSession *session, FBSessionState state, NSError *error) {
+                                      [self sessionStateChanged:session state:state error:error];
+                                  }];
+    
     [FBSession openActiveSessionWithPublishPermissions:[[NSArray alloc] initWithObjects:@"publish_actions", nil] defaultAudience:FBSessionDefaultAudienceEveryone allowLoginUI:YES completionHandler:^(FBSession *session,
                                                                                                                                                                                     FBSessionState state, NSError *error) {
         [self sessionStateChanged:session state:state error:error];
     }];
-    /*[FBSession openActiveSessionWithReadPermissions:[[NSArray alloc] initWithObjects:@"publish_actions", nil]
+    [FBSession openActiveSessionWithReadPermissions:[[NSArray alloc] initWithObjects:@"publish_actions", nil]
                                        allowLoginUI:YES
                                   completionHandler:
      ^(FBSession *session,
@@ -95,9 +117,45 @@
 
 -(void)publishStory:(NSDictionary*)post
 {
-    [FBSettings setLoggingBehavior:[NSSet setWithObjects:FBLoggingBehaviorFBRequests, FBLoggingBehaviorFBURLConnections, nil]];
+    if(FBSession.activeSession.isOpen)
+    {
+        NSLog(@"test");
+    }
+    else{
+        NSLog(@"bad");
+    }
+    
+    NSArray* permissions = [NSArray arrayWithObjects:@"publish_actions", nil];
+     
+     [[FBSession activeSession] reauthorizeWithPublishPermissions:permissions
+                                                  defaultAudience:FBSessionDefaultAudienceFriends
+                                                completionHandler:^(FBSession *session, NSError *error) {
+                                                    /* handle success + failure in block */
+            }];
+    
+    BOOL displayedNativeDialog = [FBNativeDialogs
+     presentShareDialogModallyFrom:self.vc
+     initialText:@""
+     image:nil
+     url:nil
+     handler:^(FBNativeDialogResult result, NSError *error) {
+         if (error) {
+             /* handle failure */
+         } else {
+             if (result == FBNativeDialogResultSucceeded) {
+                 /* handle success */
+             } else {
+                 /* handle user cancel */
+             }
+         }
+     }];
+    if (!displayedNativeDialog) {
+        /* handle fallback to native dialog  */
+    }
+     
+   /*[FBSettings setLoggingBehavior:[NSSet setWithObjects:FBLoggingBehaviorFBRequests, FBLoggingBehaviorFBURLConnections, nil]];
     [FBRequestConnection
-     startWithGraphPath:@"me/feed"
+     startWithGraphPath:@"me/MyCastle"
      parameters:post
      HTTPMethod:@"POST"
      completionHandler:^(FBRequestConnection *connection,
@@ -121,6 +179,65 @@
                            otherButtonTitles:nil]
           show];
      }];
+    
+    [FBRequestConnection startForPostWithGraphPath:@"me/fb_sample_scrumps:eat"
+                                       graphObject:id<FBGraphObject>
+                                 completionHandler:^(FBRequestConnection *connection,
+                                                     id result,
+                                                     NSError *error) {
+                                     if (!error) {
+                                         [[[UIAlertView alloc] initWithTitle:@"Result"
+                                                                     message:[NSString stringWithFormat:@"Posted Open Graph action, id: %@",
+                                                                              [result objectForKey:@"id"]]
+                                                                    delegate:nil
+                                                           cancelButtonTitle:@"Thanks!"
+                                                           otherButtonTitles:nil]
+                                          show];
+                                         
+                                         // start over
+                                         self.selectedMeal = nil;
+                                         self.selectedPlace = nil;
+                                         self.selectedFriends = nil;
+                                         [self updateSelections];
+                                     } else {
+                                         // do we lack permissions here? If so, the application's policy is to reask for the permissions, and if
+                                         // granted, we will recall this method in order to post the action
+                                         if ([[error userInfo][FBErrorParsedJSONResponseKey][@"body"][@"error"][@"code"] compare:@200] == NSOrderedSame) {
+                                             [FBSession.activeSession reauthorizeWithPublishPermissions:[NSArray arrayWithObject:@"publish_actions"]
+                                                                                        defaultAudience:FBSessionDefaultAudienceFriends
+                                                                                      completionHandler:^(FBSession *session, NSError *innerError) {
+                                                                                          if (!innerError) {
+                                                                                              // re-call assuming we now have the permission
+                                                                                              [self postOpenGraphAction];
+                                                                                          }
+                                                                                          else{
+                                                                                              // If we are here, this means the user has disallowed posting after a retry
+                                                                                              // which means iOS 6.0 will have turned the app's slider to "off" in the
+                                                                                              // device settings->Facebook.
+                                                                                              // You may want to customize the message for your application, since this
+                                                                                              // string is specifically for iOS 6.0.
+                                                                                              [[[UIAlertView alloc] initWithTitle:@"Permission To Post Disallowed"
+                                                                                                                          message:@"Use device settings->Facebook to re-enable permission to post."
+                                                                                                                         delegate:nil
+                                                                                                                cancelButtonTitle:@"Thanks!"
+                                                                                                                otherButtonTitles:nil]
+                                                                                               show];
+                                                                                          }
+                                                                                      }];
+                                         } else {
+                                             [[[UIAlertView alloc] initWithTitle:@"Result"
+                                                                         message:[NSString stringWithFormat:@"error: domain = %@, code = %@(%d)",
+                                                                                  error.domain,
+                                                                                  [SCAppDelegate FBErrorCodeDescription:error.code],
+                                                                                  error.code]
+                                                                        delegate:nil
+                                                               cancelButtonTitle:@"Thanks!"
+                                                               otherButtonTitles:nil]
+                                              show];
+                                         }
+                                     }
+                                 }];*/
+
 }
 
 @end
